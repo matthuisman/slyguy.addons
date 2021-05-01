1f9fdf67077f19e735fe44b62aa10dee1729c673
Matt Huisman <hi@matthuisman.nz>
2021-05-02 09:33:24 +1200

    plugin.program.iptv.merge > v0.15.9

--- plugin.program.iptv.merge/resources/lib/service.py
+++ plugin.program.iptv.merge/resources/lib/service.py
@@ -1,5 +1,4 @@
 import time
-import sqlite3
 
 from kodi_six import xbmc, xbmcvfs, xbmcaddon
 from six.moves.urllib.parse import unquote
@@ -10,30 +9,6 @@ from slyguy.util import get_kodi_string, set_kodi_string, kodi_rpc, kodi_db
 from slyguy.log import log
 
 from .constants import *
-
-def _clean_tables(db_name, tables):
-    if not tables or not db_name:
-        return
-
-    db_path = kodi_db(db_name)
-    if not db_path:
-        return
-
-    conn = sqlite3.connect(db_path, isolation_level=None)
-    try:
-        c = conn.cursor()
-
-        for table in tables:
-            c.execute("DELETE FROM {};".format(table))
-
-        c.execute("VACUUM;")
-        conn.commit()
-    except:
-        raise
-    else:
-        log.debug('DB Cleaned: {}'.format(db_path))
-    finally:
-        conn.close()
 
 def start():
     monitor = xbmc.Monitor()
@@ -61,9 +36,13 @@ def start():
         if restart_queued and settings.getBool('restart_pvr', False):
             if forced: progress = gui.progressbg(heading='Reloading IPTV Simple Client')
 
-            if forced or (not xbmc.getCondVisibility('Pvr.IsPlayingTv') and not xbmc.getCondVisibility('Pvr.IsPlayingRadio')):
+            if KODI_VERSION > 18:
                 restart_queued = False
+                try: xbmcaddon.Addon(IPTV_SIMPLE_ID).setSetting('m3uPathType', '0')
+                except Exception as e: pass
 
+            elif forced or (not xbmc.getCondVisibility('Pvr.IsPlayingTv') and not xbmc.getCondVisibility('Pvr.IsPlayingRadio')):
+                restart_queued = False
                 kodi_rpc('Addons.SetAddonEnabled', {'addonid': IPTV_SIMPLE_ID, 'enabled': False})
 
                 wait_delay = 4
@@ -71,12 +50,6 @@ def start():
                     if monitor.waitForAbort(1):
                         break
                     if forced: progress.update((i+1)*int(100/wait_delay))
-
-                if settings.getBool('clean_dbs', True):
-                    try: _clean_tables('tv', ['channelgroups', 'channels', 'map_channelgroups_channels'])
-                    except: pass
-                    try: _clean_tables('epg', ['epg', 'epgtags', 'lastepgscan'])
-                    except: pass
 
                 kodi_rpc('Addons.SetAddonEnabled', {'addonid': IPTV_SIMPLE_ID, 'enabled': True})
 
