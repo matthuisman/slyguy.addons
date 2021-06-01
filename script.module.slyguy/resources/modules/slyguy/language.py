f30320c3980ed21a07906bc84c59b0999906ee47
johnny5-is-alive <github.johnny5@gmail.com>
2021-06-02 09:59:45 +1200

    script.module.slyguy > v0.31.7

--- script.module.slyguy/resources/modules/slyguy/language.py
+++ script.module.slyguy/resources/modules/slyguy/language.py
@@ -159,7 +159,6 @@ class BaseLanguage(object):
     INSTALLING_APT_IA           = 32119
     FOLDERS_FOR_SHOW_SERIES     = 32120
     IA_TESTING_NOT_AVAILABLE    = 32121
-    IA_TCMALLOC_REQUIRED        = 32122
 
     def __getattribute__(self, name):
         attr = object.__getattribute__(self, name)
