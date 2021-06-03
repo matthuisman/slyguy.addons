2ed693273c627724c67ffe5245aadb8868deebe8
johnny5-is-alive <github.johnny5@gmail.com>
2021-06-03 21:19:49 +1200

    plugin.video.foxtel.go > v0.7.7

--- plugin.video.foxtel.go/resources/lib/constants.py
+++ plugin.video.foxtel.go/resources/lib/constants.py
@@ -1,21 +1,17 @@
-from slyguy.constants import ADDON_ID
-
-if ADDON_ID == 'plugin.video.foxtel.now':
-    APP_ID = 'PLAY2'
-    URL    = 'now-mobile-140'
-else:
-    APP_ID = 'GO2'
-    URL    = 'go-mobile-440'
-
 HEADERS = {
-    'User-Agent': 'okhttp/2.7.5',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36',
 }
 
-BASE_URL     = 'https://foxtel-go-sw.foxtelplayer.foxtel.com.au/{}'.format(URL)
-API_URL      = BASE_URL + '/api{}'
-BUNDLE_URL   = BASE_URL + '/bundleAPI/getHomeBundle.php'
-IMG_URL      = BASE_URL + '/imageHelper.php?id={id}:png&w={width}{fragment}'
-SEARCH_URL   = 'https://foxtel-prod-elb.digitalsmiths.net/sd/foxtel/taps/assets/search/prefix'
+LIVE_SITEID = '206'
+VOD_SITEID  = '296'
+
+BASE_URL = 'https://foxtel-go-sw.foxtelplayer.foxtel.com.au/go-mobile-570'
+API_URL = BASE_URL + '/api{}'
+BUNDLE_URL = BASE_URL + '/bundleAPI/getHomeBundle.php'
+IMG_URL = BASE_URL + '/imageHelper.php?id={id}:png&w={width}{fragment}'
+SEARCH_URL = 'https://foxtel-prod-elb.digitalsmiths.net/sd/foxtel/taps/assets/search/prefix'
+EPG_URL = 'https://foxtel-go-sw.foxtelplayer.foxtel.com.au/go-mobile-440/api/epg.class.api.php/getChannelListings/' + LIVE_SITEID
+PLAYBACK_URL = 'https://foxtel-go-sw.foxtelplayer.foxtel.com.au/now-box-140/api/playback.class.api.php/{endpoint}/{site_id}/1/{id}'
 
 AES_IV = 'b2d40461b54d81c8c6df546051370328'
 PLT_DEVICE = 'andr_phone'
@@ -23,9 +19,6 @@ EPG_EVENTS_COUNT = 6
 
 TYPE_LIVE  = '1'
 TYPE_VOD   = '2'
-
-LIVE_SITEID = '206'
-VOD_SITEID  = '296'
 
 ASSET_MOVIE  = '1'
 ASSET_TVSHOW = '4'
