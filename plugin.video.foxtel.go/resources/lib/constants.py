2e46904bd6f70c46ad39fbcc0b6bc547aee78b29
johnny5-is-alive <github.johnny5@gmail.com>
2021-06-04 20:57:09 +1200

    plugin.video.foxtel.go > v0.8.1

--- plugin.video.foxtel.go/resources/lib/constants.py
+++ plugin.video.foxtel.go/resources/lib/constants.py
@@ -11,10 +11,10 @@ BUNDLE_URL = BASE_URL + '/bundleAPI/getHomeBundle.php'
 IMG_URL = BASE_URL + '/imageHelper.php?id={id}:png&w={width}{fragment}'
 SEARCH_URL = 'https://foxtel-prod-elb.digitalsmiths.net/sd/foxtel/taps/assets/search/prefix'
 EPG_URL = 'https://foxtel-go-sw.foxtelplayer.foxtel.com.au/go-mobile-440/api/epg.class.api.php/getChannelListings/' + LIVE_SITEID
+LEGACY_PLAY = 'https://foxtel-go-sw.foxtelplayer.foxtel.com.au/now-mobile-140/api/playback.class.api.php/{endpoint}/{site_id}/1/{id}'
 
 AES_IV = 'b2d40461b54d81c8c6df546051370328'
 PLT_DEVICE = 'andr_screen'
-LEGACY_DEVICE = 'andr_phone'
 EPG_EVENTS_COUNT = 6
 
 TYPE_LIVE  = '1'
