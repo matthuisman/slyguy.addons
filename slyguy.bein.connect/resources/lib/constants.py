70dde0cc62b2089a017ad2fb207e348eb5cd4f3a
Matt Huisman <hi@matthuisman.nz>
2021-06-09 16:12:04 +1200

    slyguy.bein.connect > v0.1.7

--- slyguy.bein.connect/resources/lib/constants.py
+++ slyguy.bein.connect/resources/lib/constants.py
@@ -5,8 +5,8 @@ HEADERS = {
 APP_DATA_URL = 'https://storage.googleapis.com/static-production.netcosports.com/bein_content/init/bein_dev_new_api.json'
 SECRET_KEY = '2H6kATruFruW&&e5_u4u'
 REGION = 'mena'
-PLAYER_VERSION = '9.5.2'
 PLAYER_NAME = 'androiddashapp'
+APP_VERSION_URL = 'https://i.mjh.nz/bein/mena.version'
 
 LOGIN_MULTI_IP = 0
 LOGIN_MULTI_DEVICE = 1
