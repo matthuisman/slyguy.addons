3b5ee3d813b7215ae17176e55d7e571e27b74e73
johnny5-is-alive <github.johnny5@gmail.com>
2021-06-14 11:21:25 +1200

    script.module.slyguy > v0.32.6

--- script.module.slyguy/resources/modules/arrow/constants.py
+++ script.module.slyguy/resources/modules/arrow/constants.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
+import sys
+import time
+from datetime import datetime
 
-# Output of time.mktime(datetime.max.timetuple()) on macOS
-# This value must be hardcoded for compatibility with Windows
-# Platform-independent max timestamps are hard to form
-# https://stackoverflow.com/q/46133223
-MAX_TIMESTAMP = 253402318799.0
+try:
+    _MAX_TIMESTAMP = time.mktime(datetime.max.timetuple())
+except (OverflowError, ValueError, OSError):
+    is_64bits = sys.maxsize > 2 ** 32
+    _MAX_TIMESTAMP = (
+        time.mktime(datetime(3000, 1, 1, 23, 59, 59, 999999).timetuple())
+        if is_64bits
+        else time.mktime(datetime(2038, 1, 1, 23, 59, 59, 999999).timetuple())
+    )
+
+MAX_TIMESTAMP = _MAX_TIMESTAMP
 MAX_TIMESTAMP_MS = MAX_TIMESTAMP * 1000
 MAX_TIMESTAMP_US = MAX_TIMESTAMP * 1000000
