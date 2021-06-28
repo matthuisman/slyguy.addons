b4fd63b907f60dd4e40b747f64031f7c27083cf6
Matt Huisman <hi@matthuisman.nz>
2021-06-29 10:26:06 +1200

    slyguy.samsung.tv.plus > v0.0.1

---
+++ slyguy.samsung.tv.plus/resources/lib/language.py
@@ -0,0 +1,16 @@
+from slyguy.language import BaseLanguage
+
+class Language(BaseLanguage):
+    LIVE_TV     = 30000
+    CH_LABEL    = 30001
+    REGION      = 30002
+    ALL         = 30003
+    US          = 30004
+    GB          = 30005
+    CA          = 30006
+    IA          = 30007
+    DE          = 30008
+    ES          = 30009
+    IT          = 30010
+
+_ = Language()
