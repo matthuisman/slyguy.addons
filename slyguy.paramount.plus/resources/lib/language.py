776b27f05f37d01e071f8f3e3dbfab8a5b9848bb
johnny5-is-alive <github.johnny5@gmail.com>
2021-06-28 15:42:17 +1200

    slyguy.paramount.plus > v0.0.8

--- slyguy.paramount.plus/resources/lib/language.py
+++ slyguy.paramount.plus/resources/lib/language.py
@@ -27,5 +27,8 @@ class Language(BaseLanguage):
     ACCESSIBILITY        = 30023
     EC3_ENABLED          = 30024
     AC3_ENABLED          = 30025
+    CANADA               = 30026
+    AUSTRALIA            = 30027
+    WV_SECURE            = 30028
 
 _ = Language()
