ef3d621dd8da17eb1ac213b81585ecc73386ee61
Matt Huisman <hi@matthuisman.nz>
2021-06-29 10:39:15 +1200

    plugin.video.skygo.nz > removed

--- plugin.video.skygo.nz/default.py
+++
@@ -1,5 +0,0 @@
-from slyguy.migrate import migrate
-
-message = 'SkyGo has made big changes & a new add-on is required.\nCurrently live channels is working. VOD coming soon.\nClick Yes to install the new add-on'
-
-migrate('slyguy.skygo.nz', copy_userdata=False, message=message)
