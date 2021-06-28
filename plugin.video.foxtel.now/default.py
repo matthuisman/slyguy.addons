ee27cbf11287f8a94cb6b9d3fe9f68b51a59897f
johnny5-is-alive <github.johnny5@gmail.com>
2021-06-29 10:39:15 +1200

    plugin.video.foxtel.now > removed

--- plugin.video.foxtel.now/default.py
+++
@@ -1,5 +0,0 @@
-from slyguy.migrate import migrate
-
-message = 'My Foxtel Go add-on now supports Foxtel Now.\nTherefore this add-on has been depreciated\nClick Yes to install Foxtel Go add-on'
-
-migrate('plugin.video.foxtel.go', copy_userdata=False, message=message)
