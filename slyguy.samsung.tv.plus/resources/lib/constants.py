cb20231e6db6a3f9c57de69f86fd54eb63361c23
Matt Huisman <hi@matthuisman.nz>
2021-07-02 13:52:00 +1200

    slyguy.samsung.tv.plus > v0.0.8

--- slyguy.samsung.tv.plus/resources/lib/constants.py
+++ slyguy.samsung.tv.plus/resources/lib/constants.py
@@ -1,21 +1,3 @@
-HEADERS = {
-    'user-agent': 'okhttp/4.9.0',
-}
-
-DATA_URL = 'https://i.mjh.nz/SamsungTVPlus/{region}.json.gz'
-EPG_URL = 'https://i.mjh.nz/SamsungTVPlus/{region}.xml.gz'
-
-ALL = 'all'
-US = 'us'
-GB = 'gb'
-CA = 'ca'
-IN = 'in'
-DE = 'de'
-ES = 'es'
-IT = 'it'
-KR = 'kr'
-FR = 'fr'
-CH = 'ch'
-AT = 'at'
-
-REGIONS = [ALL, US, GB, CA, IN, DE, ES, IT, KR, FR, CH, AT]
+DATA_URL = 'https://i.mjh.nz/SamsungTVPlus/app.json.gz'
+EPG_URL = 'https://i.mjh.nz/SamsungTVPlus/all.xml.gz'
+ALL = '_'
--- slyguy.samsung.tv.plus/resources/lib/plugin.py
+++ slyguy.samsung.tv.plus/resources/lib/plugin.py
@@ -1,7 +1,7 @@
 import codecs
 
 import arrow
-from slyguy import plugin, inputstream, mem_cache, settings
+from slyguy import plugin, inputstream, mem_cache, settings, userdata, gui
 from slyguy.session import Session
 from slyguy.util import gzip_extract
 
@@ -21,17 +21,109 @@ def home(**kwargs):
 
     return folder
 
-def _channels():
-    @mem_cache.cached(60*5)
-    def fetch_channels(region):
-        return Session().gz_json(DATA_URL.format(region=region))
+@mem_cache.cached(60*15)
+def _data():
+    return Session().gz_json(DATA_URL)
 
-    region = settings.getEnum('region', REGIONS, default=ALL)
-    return fetch_channels(region)
+def _app_data():
+    data = _data()
+
+    region = {'logo': None, 'name':_.ALL, 'channels': {}, 'sort': 0}
+    for key in data['regions']:
+        data['regions'][key]['sort'] = 1
+        region['channels'].update(data['regions'][key]['channels'])
+
+    data['regions'][ALL] = region
+
+    return data
 
 @plugin.route()
-def live_tv(**kwargs):
-    folder = plugin.Folder(_.LIVE_TV)
+def toggle_merge(code, **kwargs):
+    data = _app_data()
+
+    regions = userdata.get('merge_regions', [])
+    region = data['regions'][code]
+
+    if code in regions:
+        if code == ALL:
+            regions = [ALL]
+
+        regions.remove(code)
+    else:
+        if code == ALL:
+            regions = []
+
+        regions.append(code)
+
+    if ALL in regions and code != ALL:
+        regions.remove(ALL)
+
+    gui.notification(_.MERGE_ADDED if code in regions else _.MERGE_REMOVED, heading=region['name'], icon=region['logo'])
+    userdata.set('merge_regions', regions)
+    gui.refresh()
+
+@plugin.route()
+def live_tv(code=None, group=None, **kwargs):
+    data = _app_data()
+    regions = userdata.get('merge_regions', [])
+
+    if not code:
+        folder = plugin.Folder(_.LIVE_TV)
+
+        for code in sorted(data['regions'], key=lambda x: (data['regions'][x]['sort'], data['regions'][x]['name'])):
+            region = data['regions'][code]
+            ch_count = len(region['channels'])
+            in_merge = code in regions
+
+            if code == ALL:
+                region['name'] = _(region['name'], _bold=True)
+
+            folder.add_item(
+                label = _(u'{name} ({count})'.format(name=region['name'], count=ch_count), _color='FF19f109' if in_merge else ''),
+                art = {'thumb': region.get('logo')},
+                info = {
+                    'plot': u'{}\n\n{}\n\n{}'.format(region['name'], _(_.CHANNEL_COUNT, count=ch_count), _(_.MERGE_INCLUDED, _color='FF19f109') if in_merge else ''),
+                },
+                context = ((_.MERGE_REMOVE if in_merge else _.MERGE_ADD, 'RunPlugin({})'.format(plugin.url_for(toggle_merge, code=code))),),
+                path = plugin.url_for(live_tv, code=code),
+            )
+
+        return folder
+
+    region = data['regions'][code]
+    folder = plugin.Folder(region['name'])
+    channels = region['channels']
+
+    if group is None:
+        groups = {}
+        all_count = 0
+        for id in channels:
+            channel = channels[id]
+            all_count += 1
+            if channel['group'] not in groups:
+                groups[channel['group']] = 1
+            else:
+                groups[channel['group']] += 1
+
+        folder = plugin.Folder(region['name'])
+
+        folder.add_item(
+            label = _(u'{name} ({count})'.format(name=_.ALL, count=all_count), _bold=True),
+            art = {'thumb': region.get('logo')},
+            path = plugin.url_for(live_tv, code=code, group=ALL),
+        )
+
+        for group in sorted(groups):
+            folder.add_item(
+                label = _(u'{name} ({count})'.format(name=group, count=groups[group])),
+                art = {'thumb': region.get('logo')},
+                path = plugin.url_for(live_tv, code=code, group=group)
+            )
+
+        return folder
+
+    folder = plugin.Folder(region['name'] if group == ALL else group)
+    show_chno = settings.getBool('show_chno', True)
 
     if settings.getBool('show_epg', True):
         now = arrow.now()
@@ -39,9 +131,11 @@ def live_tv(**kwargs):
     else:
         EPG_EVENTS_COUNT = None
 
-    channels = _channels()
-    for id in sorted(channels.keys(), key=lambda x: channels[x]['chno']):
+    for id in sorted(channels.keys(), key=lambda x: channels[x]['chno'] if show_chno else channels[x]['name']):
         channel = channels[id]
+
+        if group != ALL and channel['group'] != group:
+            continue
         
         if not EPG_EVENTS_COUNT:
             plot = channel['description']
@@ -60,7 +154,7 @@ def live_tv(**kwargs):
                         break
 
         folder.add_item(
-            label = _(_.CH_LABEL, chno=channel['chno'], name=channel['name']),
+            label = u'{} | {}'.format(channel['chno'], channel['name']) if show_chno else channel['name'],
             info = {'plot': plot},
             art = {'thumb': channel['logo']},
             playable = True,
@@ -71,33 +165,39 @@ def live_tv(**kwargs):
 
 @plugin.route()
 def play(id, **kwargs):
-    channel = _channels()[id]
+    data = _app_data()
+    channel = data['regions'][ALL]['channels'][id]
+
     return plugin.Item(
         label = channel['name'],
         info = {'plot': channel['description']},
         art = {'thumb': channel['logo']},
         inputstream = inputstream.HLS(live=True),
-        headers = HEADERS,
+        headers = data['headers'],
         path = channel['url'],
     )
 
 @plugin.route()
 @plugin.merge()
 def playlist(output, **kwargs):
-    channels = _channels()
+    data = _app_data()
+    data['regions'].pop(ALL, None)
+
+    regions = userdata.get('merge_regions', [])
+    if ALL in regions:
+        regions = [x for x in data['regions']]
+    else:
+        regions = [x for x in regions if x in data['regions']]
+
     with codecs.open(output, 'w', encoding='utf8') as f:
         f.write(u'#EXTM3U')
 
-        for id in sorted(channels.keys(), key=lambda x: channels[x]['chno']):
-            channel = channels[id]
-            f.write(u'\n#EXTINF:-1 tvg-id="{id}" tvg-chno="{chno}" tvg-name="{name}" tvg-logo="{logo}" group-title="{group}",{name}\n{url}'.format(
-                id=id, chno=channel['chno'], name=channel['name'], logo=channel['logo'], group=channel['group'], url=plugin.url_for(play, id=id, _is_live=True),
-            ))
+        for code in regions:
+            region = data['regions'][code]
+            channels = region['channels']
 
-@plugin.route()
-@plugin.merge()
-def epg(output, **kwargs):
-    epg_url = EPG_URL.format(region=settings.getEnum('region', REGIONS, default=ALL))
-    Session().chunked_dl(epg_url, output)
-    if epg_url.endswith('.gz'):
-        gzip_extract(output)
+            for id in sorted(channels.keys(), key=lambda x: channels[x]['chno']):
+                channel = channels[id]
+                f.write(u'\n#EXTINF:-1 tvg-id="{id}" tvg-chno="{chno}" tvg-name="{name}" tvg-logo="{logo}" group-title="{region};{group}",{name}\n{url}'.format(
+                    id=id, chno=channel['chno'], name=channel['name'], logo=channel['logo'], region=region['name'], group=channel['group'], url=plugin.url_for(play, id=id, _is_live=True),
+                ))
