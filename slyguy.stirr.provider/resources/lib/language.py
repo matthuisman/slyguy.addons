f934dec6d8507352e30ac26280594ca68568b082
Matt Huisman <hi@matthuisman.nz>
2021-07-06 13:03:44 +1200

    slyguy.stirr.provider > v0.1.6

--- slyguy.stirr.provider/resources/lib/language.py
+++ slyguy.stirr.provider/resources/lib/language.py
@@ -1,7 +1,8 @@
 from slyguy.language import BaseLanguage
 
 class Language(BaseLanguage):
-    LIVE_TV     = 30000
-    CH_LABEL    = 30001
+    LIVE_TV       = 30000
+    SHOW_MINI_EPG = 30001
+    SHOW_CHNOS    = 30002
 
 _ = Language()
