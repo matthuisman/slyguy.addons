9387248ef152e4ca1da4e5c1fc3e67d789c9ecd8
Matt Huisman <hi@matthuisman.nz>
2021-07-13 12:38:29 +1200

    slyguy.7plus > v0.0.8

--- slyguy.7plus/resources/lib/plugin.py
+++ slyguy.7plus/resources/lib/plugin.py
@@ -336,7 +336,6 @@ def _play(account, reference, live=False):
 
     if live and item.inputstream:
         item.inputstream.live = True
-        item.inputstream.x_discontinuity = True
 
     return item
 
