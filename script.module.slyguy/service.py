6881302f4446fddaa214f20b672a8da0fbbdbe60
Matt Huisman <hi@matthuisman.nz>
2021-07-23 08:46:03 +1200

    script.module.slyguy > v0.36.4

--- script.module.slyguy/service.py
+++ script.module.slyguy/service.py
@@ -4,6 +4,12 @@ import sys
 path = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(1, os.path.realpath(os.path.join(path, 'resources/modules')))
 
-from resources.lib import service
-
-service.start()
+try:
+    from resources.lib import service
+    service.start()
+except Exception as e:
+    import xbmc, xbmcgui
+    xbmc.log('Failed to import Slyguy common service', xbmc.LOGFATAL)
+    xbmc.log('Updating add-ons', xbmc.LOGDEBUG)
+    xbmc.executebuiltin('UpdateAddonRepos')
+    xbmcgui.Dialog().ok('SlyGuy Error', 'Error starting Slyguy common service.\nPlease restart kodi and try again.')
