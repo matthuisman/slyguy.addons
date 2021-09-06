dbc61294331f275a939dbb3e7dcb02832af87492
Matt Huisman <hi@matthuisman.nz>
2021-09-06 12:30:44 +1200

    script.module.slyguy > v0.42.8

--- script.module.slyguy/resources/modules/slyguy/util.py
+++ script.module.slyguy/resources/modules/slyguy/util.py
@@ -242,11 +242,25 @@ def FileIO(file_name, method, chunksize=CHUNK_SIZE):
     else:
         return open(file_name, method, chunksize)
 
+def same_file(path_a, path_b):
+    if path_a.lower().strip() == path_b.lower().strip():
+        return True
+
+    stat_a = os.stat(path_a) if os.path.isfile(path_a) else None
+    if not stat_a:
+        return False
+
+    stat_b = os.stat(path_b) if os.path.isfile(path_b) else None
+    if not stat_b:
+        return False
+
+    return (stat_a.st_dev == stat_b.st_dev) and (stat_a.st_ino == stat_b.st_ino) and (stat_a.st_mtime == stat_b.st_mtime)
+
 def _safe_copy(src, dst, del_src=False):
     src = xbmc.translatePath(src)
     dst = xbmc.translatePath(dst)
 
-    if not xbmcvfs.exists(src):
+    if not xbmcvfs.exists(src) or same_file(src, dst):
         return
 
     if xbmcvfs.exists(dst):
