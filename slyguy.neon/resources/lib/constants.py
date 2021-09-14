8f2ab2cce7bedc12435a9dbd6b0d21a211dc3d96
Matt Huisman <hi@matthuisman.nz>
2021-09-15 10:10:24 +1200

    slyguy.neon > v0.1.3

--- slyguy.neon/resources/lib/constants.py
+++ slyguy.neon/resources/lib/constants.py
@@ -1,13 +1,14 @@
 HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (CrKey armv7l 1.5.16041) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.0 Safari/537.36', 
-    'X-Forwarded-For' : '202.89.4.222',
+    "User-Agent": "Mozilla/5.0 (CrKey armv7l 1.5.16041) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.0 Safari/537.36",
 }
 
-API_URL            = 'https://api.neontv.co.nz/api/client/gql'
+API_URL = "https://api.neontv.co.nz/api/client/gql"
 
-BRIGHTCOVE_URL     = 'https://edge.api.brightcove.com/playback/v1/accounts/{}/videos/ref:{}?jwtauth={}'
-BRIGHTCOVE_ACCOUNT = '5449166731001'
-BRIGHTCOVE_KEY     = 'BCpkADawqM31jJSqDf-CU0kRN8vMu46gUZEzcsZuB6MCCF3tx37IuORLwSj3IGTghvxSyTplhz9kRrSCm3zBR1S2UExgefbeYzdgIpcoEmU3VV-loNsg_BV7pdsVMz6_Am0cz_sS6mOG2jeT'
+BRIGHTCOVE_URL = (
+    "https://edge.api.brightcove.com/playback/v1/accounts/{}/videos/ref:{}?jwtauth={}"
+)
+BRIGHTCOVE_ACCOUNT = "5449166731001"
+BRIGHTCOVE_KEY = "BCpkADawqM31jJSqDf-CU0kRN8vMu46gUZEzcsZuB6MCCF3tx37IuORLwSj3IGTghvxSyTplhz9kRrSCm3zBR1S2UExgefbeYzdgIpcoEmU3VV-loNsg_BV7pdsVMz6_Am0cz_sS6mOG2jeT"
 
-TV_ID      = '/tv-series/all'
-MOVIES_ID  = '/browse-movies/all-movies'
+TV_ID = "/tv-series/all"
+MOVIES_ID = "/browse-movies/all-movies"
