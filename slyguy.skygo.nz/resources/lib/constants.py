2a8899bc0ee64af54d342434d0e1d16448f0b177
Matt Huisman <hi@matthuisman.nz>
2021-09-25 12:11:36 +1200

    slyguy.skygo.nz > v0.2.0

--- slyguy.skygo.nz/resources/lib/constants.py
+++ slyguy.skygo.nz/resources/lib/constants.py
@@ -1,5 +1,6 @@
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (CrKey armv7l 1.5.16041) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.0 Safari/537.36 sky-android (ver=1.0)',
+    'X-Forwarded-For': '202.89.4.222',
 }
 
 CLIENT_ID = 'dXhXjmK9G90mOX3B02R1kV7gsC4bp8yx'
