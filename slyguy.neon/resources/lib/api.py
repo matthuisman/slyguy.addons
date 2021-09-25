6a01c53672ed74fbdaa54abba62d25ccb930bfa3
Matt Huisman <hi@matthuisman.nz>
2021-09-25 12:11:36 +1200

    slyguy.neon > v0.1.4

--- slyguy.neon/resources/lib/api.py
+++ slyguy.neon/resources/lib/api.py
@@ -6,19 +6,11 @@ from slyguy.session import Session
 from slyguy.exceptions import Error
 from slyguy.util import jwt_data
 
-from .constants import (
-    API_URL,
-    HEADERS,
-    BRIGHTCOVE_URL,
-    BRIGHTCOVE_ACCOUNT,
-    BRIGHTCOVE_KEY,
-)
+from .constants import API_URL, HEADERS, BRIGHTCOVE_URL, BRIGHTCOVE_ACCOUNT, BRIGHTCOVE_KEY
 from . import queries
-
 
 class APIError(Error):
     pass
-
 
 class API(object):
     def new_session(self):
@@ -28,91 +20,91 @@ class API(object):
         self._set_authentication()
 
     def _set_authentication(self):
-        token = userdata.get("jwt_token")
+        token = userdata.get('jwt_token')
         if not token:
             return
 
-        self._session.headers.update({"Authorization": "Bearer {}".format(token)})
+        self._session.headers.update({'Authorization': 'Bearer {}'.format(token)})
         self.logged_in = True
 
     def _device_info(self, username):
         return {
-            "uuid": hashlib.sha1(username.encode("utf8")).hexdigest(),
-            "platform": "Android",
-            "name": "KODI",
-            "os": "KODI",
-            "model": "KODI",
+            'uuid': hashlib.sha1(username.encode('utf8')).hexdigest(),
+            'platform': 'Android',
+            'name': 'KODI',
+            'os': 'KODI',
+            'model': 'KODI',
             #'version': '27'
         }
 
     def _query_request(self, query, variables=None, **kwargs):
         data = {
-            "query": " ".join(query.split()),
-            "variables": variables or {},
+            'query': ' '.join(query.split()),
+            'variables': variables or {},
         }
 
         return self._session.post(API_URL, json=data, **kwargs).json()
 
     def _set_token(self, jwt_token):
-        userdata.set("jwt_token", jwt_token)
+        userdata.set('jwt_token', jwt_token)
         data = jwt_data(jwt_token)
-        userdata.set("expires", data["exp"] - 2592000)
+        userdata.set('expires', data['exp'] - 2592000)
         self._set_authentication()
 
     def login(self, username, password):
         self.logout()
 
         variables = {
-            "input": {
-                "deviceInfo": self._device_info(username),
+            'input': {
+                'deviceInfo': self._device_info(username),
             },
-            "username": username,
-            "password": password,
+            'username': username,
+            'password': password,
         }
 
         data = self._query_request(queries.LOGIN, variables)
-        if data.get("errors"):
-            raise APIError(data["errors"][0].get("message"))
+        if data.get('errors'):
+            raise APIError(data['errors'][0].get('message'))
 
-        self._set_token(data["data"]["login"]["session"]["token"])
+        self._set_token(data['data']['login']['session']['token'])
 
     def content(self, screen_id):
         self._check_token()
 
         variables = {
-            "screenId": screen_id,
+            'screenId': screen_id,
         }
 
-        return self._query_request(queries.CONTENT, variables)["data"]["screen"]
+        return self._query_request(queries.CONTENT, variables)['data']['screen']
 
     def _check_token(self):
-        if time.time() < userdata.get("expires", 0):
+        if time.time() < userdata.get('expires', 0):
             return
 
-        self.set_profile(userdata.get("profile_id"))
+        self.set_profile(userdata.get('profile_id'))
 
     def account(self):
         self._check_token()
 
-        return self._query_request(queries.ACCOUNT)["data"]["account"]
+        return self._query_request(queries.ACCOUNT)['data']['account']
 
     def set_profile(self, profile_id):
         variables = {
-            "input": {
-                "selectedProfile": profile_id,
+            'input': {
+                'selectedProfile': profile_id,
             },
-            "pin": None,
+            'pin': None,
         }
 
-        data = self._query_request(queries.UPDATE_ACCOUNT, variables)["data"]["account"]
-        self._set_token(data["session"]["token"])
+        data = self._query_request(queries.UPDATE_ACCOUNT, variables)['data']['account']
+        self._set_token(data['session']['token'])
 
-        for row in data["profiles"]:
-            if row["id"] == data["selectedProfile"]:
-                userdata.set("profile_id", row["id"])
-                userdata.set("profile_name", row["name"])
-                userdata.set("profile_icon", row["avatar"]["uri"])
-                userdata.set("profile_kids", row["isKid"])
+        for row in data['profiles']:
+            if row['id'] == data['selectedProfile']:
+                userdata.set('profile_id', row['id'])
+                userdata.set('profile_name', row['name'])
+                userdata.set('profile_icon', row['avatar']['uri'])
+                userdata.set('profile_kids', row['isKid'])
                 return
 
         raise APIError(_.PROFILE_SET_ERROR)
@@ -121,18 +113,16 @@ class API(object):
         self._check_token()
 
         variables = {
-            "input": {
-                "query": query,
+            'input': {
+                'query': query,
             },
         }
 
-        data = self._query_request(queries.SEARCH, variables)["data"]["search"][
-            "components"
-        ]
+        data = self._query_request(queries.SEARCH, variables)['data']['search']['components']
 
         results = []
         for row in data:
-            results.extend(row["tiles"])
+            results.extend(row['tiles'])
 
         return results
 
@@ -140,25 +130,21 @@ class API(object):
         self._check_token()
 
         variables = {
-            "contentItemId": contentID,
+            'contentItemId': contentID,
         }
 
         return self._query_request(queries.PLAYBACK_AUTH, variables)
 
     def get_brightcove_src(self, referenceID, jwt_token):
-        brightcove_url = BRIGHTCOVE_URL.format(
-            BRIGHTCOVE_ACCOUNT, referenceID, jwt_token
-        )
-        data = self._session.get(
-            brightcove_url, headers={"BCOV-POLICY": BRIGHTCOVE_KEY}
-        ).json()
+        brightcove_url = BRIGHTCOVE_URL.format(BRIGHTCOVE_ACCOUNT, referenceID, jwt_token)
+        data = self._session.get(brightcove_url, headers={'BCOV-POLICY': BRIGHTCOVE_KEY}).json()
         return util.process_brightcove(data)
 
     def logout(self):
-        userdata.delete("jwt_token")
-        userdata.delete("expires")
-        userdata.delete("profile_id")
-        userdata.delete("profile_name")
-        userdata.delete("profile_icon")
-        userdata.delete("profile_kids")
+        userdata.delete('jwt_token')
+        userdata.delete('expires')
+        userdata.delete('profile_id')
+        userdata.delete('profile_name')
+        userdata.delete('profile_icon')
+        userdata.delete('profile_kids')
         self.new_session()
--- slyguy.neon/resources/lib/plugin.py
+++ slyguy.neon/resources/lib/plugin.py
@@ -6,70 +6,41 @@ from .constants import HEADERS, TV_ID, MOVIES_ID
 
 api = API()
 
-
 @signals.on(signals.BEFORE_DISPATCH)
 def before_dispatch():
     api.new_session()
     plugin.logged_in = api.logged_in
 
-
-@plugin.route("")
+@plugin.route('')
 def home(**kwargs):
     folder = plugin.Folder()
 
     if not api.logged_in:
-        folder.add_item(
-            label=_(_.LOGIN, _bold=True), path=plugin.url_for(login), bookmark=False
-        )
+        folder.add_item(label=_(_.LOGIN, _bold=True), path=plugin.url_for(login), bookmark=False)
     else:
-        folder.add_item(
-            label=_(_.TV, _bold=True),
-            path=plugin.url_for(menu, screen_id=TV_ID, title=_.TV),
-        )
-        folder.add_item(
-            label=_(_.MOVIES, _bold=True),
-            path=plugin.url_for(menu, screen_id=MOVIES_ID, title=_.MOVIES),
-        )
+        folder.add_item(label=_(_.TV,  _bold=True), path=plugin.url_for(menu, screen_id=TV_ID, title=_.TV))
+        folder.add_item(label=_(_.MOVIES,  _bold=True), path=plugin.url_for(menu, screen_id=MOVIES_ID, title=_.MOVIES))
         folder.add_item(label=_(_.SEARCH, _bold=True), path=plugin.url_for(search))
 
-        if settings.getBool("bookmarks", True):
-            folder.add_item(
-                label=_(_.BOOKMARKS, _bold=True),
-                path=plugin.url_for(plugin.ROUTE_BOOKMARKS),
-                bookmark=False,
-            )
+        if settings.getBool('bookmarks', True):
+            folder.add_item(label=_(_.BOOKMARKS, _bold=True), path=plugin.url_for(plugin.ROUTE_BOOKMARKS), bookmark=False)
 
-        if not userdata.get("kid_lockdown", False):
-            folder.add_item(
-                label=_.SELECT_PROFILE,
-                path=plugin.url_for(select_profile),
-                art={"thumb": userdata.get("profile_icon")},
-                info={"plot": userdata.get("profile_name")},
-                _kiosk=False,
-                bookmark=False,
-            )
+        if not userdata.get('kid_lockdown', False):
+            folder.add_item(label=_.SELECT_PROFILE, path=plugin.url_for(select_profile), art={'thumb': userdata.get('profile_icon')}, info={'plot': userdata.get('profile_name')}, _kiosk=False, bookmark=False)
 
-        folder.add_item(
-            label=_.LOGOUT, path=plugin.url_for(logout), _kiosk=False, bookmark=False
-        )
+        folder.add_item(label=_.LOGOUT, path=plugin.url_for(logout), _kiosk=False, bookmark=False)
 
-    folder.add_item(
-        label=_.SETTINGS,
-        path=plugin.url_for(plugin.ROUTE_SETTINGS),
-        _kiosk=False,
-        bookmark=False,
-    )
+    folder.add_item(label=_.SETTINGS, path=plugin.url_for(plugin.ROUTE_SETTINGS), _kiosk=False, bookmark=False)
 
     return folder
 
-
 @plugin.route()
 def login(**kwargs):
-    username = gui.input(_.ASK_USERNAME, default=userdata.get("username", "")).strip()
+    username = gui.input(_.ASK_USERNAME, default=userdata.get('username', '')).strip()
     if not username:
         return
 
-    userdata.set("username", username)
+    userdata.set('username', username)
 
     password = gui.input(_.ASK_PASSWORD, hide_input=True).strip()
     if not password:
@@ -79,20 +50,18 @@ def login(**kwargs):
     _select_profile()
     gui.refresh()
 
-
 @plugin.route()
 @plugin.login_required()
 def select_profile(**kwargs):
-    if userdata.get("kid_lockdown", False):
+    if userdata.get('kid_lockdown', False):
         return
 
     _select_profile()
     gui.refresh()
 
-
 def _select_profile():
     account = api.account()
-    profiles = account["profiles"]
+    profiles = account['profiles']
 
     options = []
     values = []
@@ -100,77 +69,65 @@ def _select_profile():
 
     for index, profile in enumerate(profiles):
         values.append(profile)
-        options.append(
-            plugin.Item(label=profile["name"], art={"thumb": profile["avatar"]["uri"]})
-        )
+        options.append(plugin.Item(label=profile['name'], art={'thumb': profile['avatar']['uri']}))
 
-        if profile["id"] == userdata.get("profile_id"):
+        if profile['id'] == userdata.get('profile_id'):
             default = index
 
-    index = gui.select(
-        _.SELECT_PROFILE, options=options, preselect=default, useDetails=True
-    )
+    index = gui.select(_.SELECT_PROFILE, options=options, preselect=default, useDetails=True)
     if index < 0:
         return
 
     _set_profile(values[index])
 
-
 def _set_profile(profile, notify=True):
-    api.set_profile(profile["id"])
+    api.set_profile(profile['id'])
 
-    if settings.getBool("kid_lockdown", False) and userdata.get("profile_kids"):
-        userdata.set("kid_lockdown", True)
+    if settings.getBool('kid_lockdown', False) and userdata.get('profile_kids'):
+        userdata.set('kid_lockdown', True)
 
     if notify:
-        gui.notification(
-            _.PROFILE_ACTIVATED,
-            heading=userdata.get("profile_name"),
-            icon=userdata.get("profile_icon"),
-        )
-
+        gui.notification(_.PROFILE_ACTIVATED, heading=userdata.get('profile_name'), icon=userdata.get('profile_icon'))
 
 @plugin.route()
 def menu(screen_id, title=None, **kwargs):
     data = api.content(screen_id)
-    folder = plugin.Folder(title or data["title"])
+    folder = plugin.Folder(title or data['title'])
 
-    # folder.add_item('Featured')
+    #folder.add_item('Featured')
 
-    for row in data["components"][0]["items"]:
+    for row in data['components'][0]['items']:
         folder.add_item(
-            label=row["name"],
-            path=plugin.url_for(content, screen_id=row["id"]),
+            label = row['name'],
+            path = plugin.url_for(content, screen_id=row['id']),
         )
 
     return folder
 
-
 @plugin.route()
 def content(screen_id, **kwargs):
     data = api.content(screen_id)
-    folder = plugin.Folder(data["title"])
-    folder.add_items(_parse_content(data["components"][0]["tiles"]))
+    folder = plugin.Folder(data['title'])
+    folder.add_items(_parse_content(data['components'][0]['tiles']))
     return folder
-
 
 def _parse_content(rows):
     items = []
 
     def _sort(x):
-        for row in x.get("sortValues", []):
-            if row["key"] == "name":
-                return row["value"].lower()
+        for row in x.get('sortValues', []):
+            if row['key'] == 'name':
+                return row['value'].lower()
 
-        return x["header"].lower()
+        return x['header'].lower()
 
-    for row in sorted(rows, key=lambda x: _sort(x)):
-        if row["contentItem"]["isComingSoon"] or row["contentItem"]["isRental"]:
+    for row in sorted(rows, key=lambda  x: _sort(x)):
+        if row['contentItem']['isComingSoon'] or row['contentItem']['isRental']:
             continue
 
-        if "/movie/" in row["contentItem"]["path"]:
+        if '/movie/' in row['contentItem']['path']:
             item = _parse_movie(row)
-        elif "/series/" in row["contentItem"]["path"]:
+        elif '/series/' in row['contentItem']['path']:
             item = _parse_show(row)
         else:
             continue
@@ -179,74 +136,69 @@ def _parse_content(rows):
 
     return items
 
-
 def _parse_movie(row):
     return plugin.Item(
-        label=row["header"],
-        info={
-            "plot": row["contentItem"]["summary"],
-            "year": row["contentItem"]["year"],
-            "duration": row["contentItem"]["duration"] * 60,
-            "mediatype": "movie",
+        label = row['header'],
+        info = {
+            'plot': row['contentItem']['summary'],
+            'year': row['contentItem']['year'],
+            'duration': row['contentItem']['duration']*60,
+            'mediatype': 'movie',
         },
-        art={
-            "thumb": row["image"],
-            "fanart": row["contentItem"]["keyart"]["uri"],
+        art = {
+            'thumb': row['image'],
+            'fanart': row['contentItem']['keyart']['uri'],
         },
-        playable=True,
-        path=plugin.url_for(play, id=row["contentItem"]["id"]),
+        playable = True,
+        path = plugin.url_for(play, id=row['contentItem']['id']),
     )
-
 
 def _parse_show(row):
     return plugin.Item(
-        label=row["header"],
-        info={
-            "plot": row["contentItem"]["description"],
-            "mediatype": "tvshow",
+        label = row['header'],
+        info = {
+            'plot': row['contentItem']['description'],
+            'mediatype': 'tvshow',
         },
-        art={
-            "thumb": row["image"],
-            "fanart": row["contentItem"]["keyart"]["uri"],
+        art = {
+            'thumb': row['image'],
+            'fanart': row['contentItem']['keyart']['uri'],
         },
-        path=plugin.url_for(show, path=row["contentItem"]["path"]),
+        path = plugin.url_for(show, path=row['contentItem']['path']),
     )
-
 
 @plugin.route()
 def show(path, **kwargs):
-    show = api.content(path)["components"][0]["series"]
-    folder = plugin.Folder(show["title"])
+    show = api.content(path)['components'][0]['series']
+    folder = plugin.Folder(show['title'])
 
-    for season in show["seasons"]:
+    for season in show['seasons']:
         folder.add_item(
-            label=_(_.SEASON, season_number=season["seasonNumber"]),
-            info={
-                "plot": season["description"],
-                "mediatype": "season",
+            label = _(_.SEASON, season_number=season['seasonNumber']),
+            info = {
+                'plot': season['description'],
+                'mediatype': 'season',
             },
-            art={
-                "thumb": show["tile"]["image"],
-                "fanart": show["keyart"]["uri"],
+            art = {
+                'thumb': show['tile']['image'],
+                'fanart': show['keyart']['uri'],
             },
-            path=plugin.url_for(episodes, path=show["path"], season_id=season["id"]),
+            path = plugin.url_for(episodes, path=show['path'], season_id=season['id']),
         )
 
     return folder
 
-
 @plugin.route()
 def episodes(path, season_id, **kwargs):
-    show = api.content(path)["components"][0]["series"]
-    folder = plugin.Folder(show["title"], fanart=show["keyart"]["uri"])
+    show = api.content(path)['components'][0]['series']
+    folder = plugin.Folder(show['title'], fanart=show['keyart']['uri'])
 
-    for season in show["seasons"]:
-        if season["id"] == season_id:
+    for season in show['seasons']:
+        if season['id'] == season_id:
             folder.add_items(_parse_episodes(show, season))
             break
 
     return folder
-
 
 @plugin.route()
 @plugin.search()
@@ -254,54 +206,51 @@ def search(query, page, **kwargs):
     rows = api.search(query)
     return _parse_content(rows), False
 
-
 def _parse_episodes(show, season):
     items = []
 
-    for episode in season["episodes"]:
-        if episode["isComingSoon"]:
+    for episode in season['episodes']:
+        if episode['isComingSoon']:
             continue
 
         item = plugin.Item(
-            label=episode["title"],
-            info={
-                "title": episode["title"],
-                "plot": episode["description"],
-                "duration": episode["duration"] * 60,
-                "tvshowtitle": show["title"],
-                "mediatype": "episode",
-                "season": int(episode["seasonNumber"]),
-                "episode": int(episode["episodeNumber"]),
+            label = episode['title'],
+            info = {
+                'title': episode['title'],
+                'plot': episode['description'],
+                'duration': episode['duration']*60,
+                'tvshowtitle': show['title'],
+                'mediatype': 'episode',
+                'season' : int(episode['seasonNumber']),
+                'episode': int(episode['episodeNumber']),
             },
-            art={
-                "thumb": episode["images"][0]["uri"],
+            art = {
+                'thumb': episode['images'][0]['uri'],
             },
-            playable=True,
-            path=plugin.url_for(play, id=episode["id"]),
+            playable = True,
+            path = plugin.url_for(play, id=episode['id']),
         )
 
         items.append(item)
 
     return items
 
-
 @plugin.route()
 @plugin.login_required()
 def play(id, **kwargs):
     data = api.playback_auth(id)
 
-    if "errors" in data:
-        msg = data["errors"][0]["message"]
+    if 'errors' in data:
+        msg = data['errors'][0]['message']
         plugin.exception(msg)
 
-    referenceID = data["data"]["playbackAuth"]["videos"][0]["referenceId"]
-    jwt_token = data["data"]["playbackAuth"]["videos"][0]["token"]
+    referenceID = data['data']['playbackAuth']['videos'][0]['referenceId']
+    jwt_token = data['data']['playbackAuth']['videos'][0]['token']
 
     item = api.get_brightcove_src(referenceID, jwt_token)
     item.headers = HEADERS
 
     return item
-
 
 @plugin.route()
 def logout(**kwargs):
@@ -309,5 +258,5 @@ def logout(**kwargs):
         return
 
     api.logout()
-    userdata.delete("kid_lockdown")
+    userdata.delete('kid_lockdown')
     gui.refresh()
