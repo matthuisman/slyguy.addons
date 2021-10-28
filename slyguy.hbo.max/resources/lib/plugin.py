6dbd00c6598507048607830761e451d420ba581d
Matt Huisman <hi@matthuisman.nz>
2021-10-28 13:46:36 +1300

    slyguy.hbo.max > v0.6.5

--- slyguy.hbo.max/resources/lib/plugin.py
+++ slyguy.hbo.max/resources/lib/plugin.py
@@ -535,7 +535,6 @@ def play(slug, **kwargs):
     item = plugin.Item(
         path = data['url'],
         inputstream = inputstream.MPD(),
-        headers = headers,
     )
 
     if 'defaultAudioSelection' in data:
@@ -545,8 +544,10 @@ def play(slug, **kwargs):
         item.proxy_data['original_language'] = data['originalAudioLanguage']
 
     if 'drm' in data:
-        item.inputstream = inputstream.Widevine(license_key=data['drm']['licenseUrl'])
+        item.inputstream = inputstream.Widevine(license_key=data['drm']['licenseUrl'], license_headers=headers)
         item.proxy_data['middleware'] = {data['url']: {'type': MIDDLEWARE_PLUGIN, 'url': plugin.url_for(mpd_request)}}
+    else:
+        item.headers = headers
 
     if settings.getBool('sync_playback', False):
         marker = api.markers([edit['playbackMarkerId'],])
@@ -607,7 +608,7 @@ def play(slug, **kwargs):
             row['url'] = '{base_url}/t/sub/{language}_{type}.xml'.format(base_url=base_url, language=row['language'], type=_type)
             log.debug('Generated subtitle url: {}'.format(row['url']))
 
-        item.subtitles.append({'url':row['url'], 'language':row['language'], 'forced': _type == 'forced', 'impaired': _type == 'sdh'})
+        item.subtitles.append({'url': row['url'], 'language': row['language'], 'forced': _type == 'forced', 'impaired': _type == 'sdh'})
 
     if settings.getBool('sync_playback', False):
         item.callback = {
