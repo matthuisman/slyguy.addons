f15cb8f75e30663d03916673b2e045803278365a
Matt Huisman <hi@matthuisman.nz>
2021-12-09 11:19:50 +1300

    slyguy.pluto.tv.provider > v0.4.9

--- slyguy.pluto.tv.provider/resources/lib/constants.py
+++ slyguy.pluto.tv.provider/resources/lib/constants.py
@@ -1,3 +1,4 @@
 DATA_URL = 'https://i.mjh.nz/PlutoTV/app.json.gz'
-ALL = '_'
+EPG_URL = 'https://i.mjh.nz/PlutoTV/{code}.xml.gz'
+ALL = 'all'
 UUID_NAMESPACE = '122e1611-0232-4336-bf43-e054c8ecd0d5'
--- slyguy.pluto.tv.provider/resources/lib/plugin.py
+++ slyguy.pluto.tv.provider/resources/lib/plugin.py
@@ -210,8 +210,10 @@ def playlist(output, **kwargs):
     if not regions:
         raise Exception(_.NO_REGIONS)
 
+    epg_urls = [EPG_URL.format(code=code) for code in regions]
+
     with codecs.open(output, 'w', encoding='utf8') as f:
-        f.write(u'#EXTM3U x-tvg-url="https://i.mjh.nz/PlutoTV/all.xml.gz"')
+        f.write(u'#EXTM3U x-tvg-url="{}"'.format(','.join(epg_urls)))
 
         for code in regions:
             region = data['regions'][code]
