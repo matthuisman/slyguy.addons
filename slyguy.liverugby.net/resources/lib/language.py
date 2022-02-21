6bb7c9496629a0e079a1692d345f397c1f0e9f42
Matt Huisman <hi@matthuisman.nz>
2022-02-21 14:39:36 +1300

    slyguy.liverugby.net > removed

--- slyguy.liverugby.net/resources/lib/language.py
+++
@@ -1,9 +0,0 @@
-from slyguy.language import BaseLanguage
-
-class Language(BaseLanguage):
-    ASK_USERNAME      = 30001
-    ASK_PASSWORD      = 30002
-    LIVE              = 30003
-    FEATURED          = 30004
-
-_ = Language()
