80c4ad5fb5a0012be522fffe2ea41f87c1913e64
Matt Huisman <hi@matthuisman.nz>
2022-03-09 12:48:06 +1300

    slyguy.paramount.plus > v0.4.4

--- slyguy.paramount.plus/resources/lib/api.py
+++ slyguy.paramount.plus/resources/lib/api.py
@@ -267,7 +267,12 @@ class API(object):
             'rows': 1,
             'begin': 0,
         }
-        return self._session.get('/v2.0/androidphone/shows/{}/videos/config/{}.json'.format(show_id, config), params=self._params(params)).json()['videoSectionMetadata'][0]
+        sections = self._session.get('/v2.0/androidphone/shows/{}/videos/config/{}.json'.format(show_id, config), params=self._params(params)).json()['videoSectionMetadata']
+        for section in sections:
+            if section['section_type'] == 'Full Episodes':
+                return section
+
+        return sections[-1]
 
     @mem_cache.cached(60*5)
     def seasons(self, show_id):
@@ -415,3 +420,22 @@ class API(object):
         userdata.delete('expires')
         mem_cache.empty()
         self.new_session(self._config)
+
+# https://github.com/matthuisman/slyguy.addons/issues/136
+# import base64
+# import os
+# import pyaes
+# import time
+
+# aes_key = '302a6a0d70a7e9b967f91d39fef3e387816e3095925ae4537bce96063311f9c5'
+# tv_secret = '6c70b33080758409'
+
+# def at_token():
+#    payload = '{}|{}'.format(int(time.time())*1000, tv_secret)
+#    key = bytes.fromhex(aes_key)
+#    iv = os.urandom(16)
+#    encrypter = pyaes.Encrypter(pyaes.AESModeOfOperationCBC(key, iv))
+#    ciphertext = encrypter.feed(payload)
+#    ciphertext += encrypter.feed()
+#    ciphertext = b'\x00\x10' + iv + ciphertext
+#    return base64.b64encode(ciphertext).decode('utf8')
