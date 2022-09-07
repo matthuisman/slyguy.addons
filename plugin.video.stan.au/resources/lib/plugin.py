d7bcc3e1a020362d5ae3463ca7c18168cb448ce0
Matt Huisman <hi@matthuisman.nz>
2022-09-08 08:26:09 +1200

    plugin.video.stan.au > v0.3.8

--- plugin.video.stan.au/resources/lib/plugin.py
+++ plugin.video.stan.au/resources/lib/plugin.py
@@ -329,12 +329,12 @@ def _process_entries(entries):
                 end_date = now.shift(hours=2)
 
             item.label = row['title']
-            item.info['plot'] = '[B]{}[/B]\n\n{}'.format(start_date.to('local').format('MMM Do h:mm A'), row['description'])
+            item.info['plot'] = u'[B]{}[/B]\n\n{}'.format(start_date.to('local').format('MMM Do h:mm A'), row['description'])
             if now < start_date:
-                item.label += ' [B][{}][/B]'.format(start_date.humanize())
+                item.label += u' [B][{}][/B]'.format(start_date.humanize())
             elif now > start_date and now < end_date:
                 is_live = True
-                item.label += ' [B][LIVE][/B]'
+                item.label += u' [B][LIVE][/B]'
 
             if 'episode' in row:
                 program_id = row['episode']['id']
