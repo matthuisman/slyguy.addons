715b040e3b53464e0fbf241c3fe304ba14f14d21
Matt Huisman <hi@matthuisman.nz>
2022-11-10 08:37:42 +1300

    plugin.video.sky.sport.now > v0.4.8

--- plugin.video.sky.sport.now/resources/lib/plugin.py
+++ plugin.video.sky.sport.now/resources/lib/plugin.py
@@ -264,7 +264,17 @@ def mpd_request(_data, _path, **kwargs):
 
     mpd = root.getElementsByTagName("MPD")[0]
     # Fixes issues of being too close to head and getting 404 error
-    mpd.setAttribute('availabilityStartTime', '1970-01-01T00:00:30Z')
+
+    seconds_diff = 0
+    utc = mpd.getElementsByTagName("UTCTiming")
+    if utc:
+        utc_time = arrow.get(utc[0].getAttribute('value'))
+        seconds_diff = max((arrow.now() - utc_time).total_seconds(), 0)
+
+    seconds_diff += 30
+    avail_start = arrow.get(mpd.getAttribute('availabilityStartTime'))
+    avail_start = avail_start.shift(seconds=seconds_diff)
+    mpd.setAttribute('availabilityStartTime', avail_start.format('YYYY-MM-DDTHH:mm:ss'+'Z'))
 
     with open(_path, 'wb') as f:
         f.write(root.toprettyxml(encoding='utf-8'))
