4b19672e53997ebceaa1bfaff35455afb457ca72
Matt Huisman <hi@matthuisman.nz>
2022-12-16 21:44:25 +1300

    plugin.video.au.freeview > v0.18.7

--- plugin.video.au.freeview/resources/lib/plugin.py
+++ plugin.video.au.freeview/resources/lib/plugin.py
@@ -40,6 +40,9 @@ def live_tv(**kwargs):
         plot = u''
         count = 0
         if epg_count:
+            if channel.get('epg_id') and not channel.get('programs'):
+                channel['programs'] = channels.get(channel['epg_id'], {}).get('programs', [])
+
             for index, row in enumerate(channel.get('programs', [])):
                 start = arrow.get(row[0])
                 try: stop = arrow.get(channel['programs'][index+1][0])
@@ -58,9 +61,7 @@ def live_tv(**kwargs):
             label = channel['name'],
             path = plugin.url_for(play, slug=slug, _is_live=True),
             info = {'plot': plot},
-            video = channel.get('video', {}),
-            audio = channel.get('audio', {}),
-            art = {'thumb': channel.get('logo')},
+            art = {'thumb': channel.get('logo'), 'fanart': channel.get('fanart')},
             playable = True,
         )
 
@@ -76,9 +77,7 @@ def play(slug, **kwargs):
         path = url or channel['mjh_master'],
         headers = channel.get('headers'),
         info = {'plot': channel.get('description')},
-        video = channel.get('video', {}),
-        audio = channel.get('audio', {}),
-        art = {'thumb': channel.get('logo')},
+        art = {'thumb': channel.get('logo'), 'fanart': channel.get('fanart')},
         proxy_data = {'cert': channel.get('cert')},
     )
 
