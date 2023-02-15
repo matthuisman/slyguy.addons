eac19d98072c45c211c65f8dc20de07508c1f6c7
Matt Huisman <hi@matthuisman.nz>
2023-02-15 21:57:06 +1300

    plugin.video.kayo.sports -> v0.13.8

--- plugin.video.kayo.sports/resources/lib/api.py
+++ plugin.video.kayo.sports/resources/lib/api.py
@@ -42,7 +42,7 @@ class API(object):
         return self._subscribed
 
     def _oauth_token(self, data, _raise=True):
-        token_data = self._session.post(AUTH_URL + '/token', json=data, headers={'User-Agent': 'okhttp/3.10.0'}, error_msg=_.TOKEN_ERROR).json()
+        token_data = self._session.post(AUTH_URL + '/token', json=data, headers={'User-Agent': 'okhttp/4.9.3'}, error_msg=_.TOKEN_ERROR).json()
 
         if 'error' in token_data:
             error = _.REFRESH_TOKEN_ERROR if data.get('grant_type') == 'refresh_token' else _.LOGIN_ERROR
