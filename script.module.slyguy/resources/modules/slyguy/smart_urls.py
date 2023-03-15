8d791324ee5b79da03b2643b7e1550ee9ca484ad
Matt Huisman <hi@matthuisman.nz>
2023-03-15 23:50:00 +1300

    script.module.slyguy -> v0.62.1

--- script.module.slyguy/resources/modules/slyguy/smart_urls.py
+++ script.module.slyguy/resources/modules/slyguy/smart_urls.py
@@ -1,10 +1,8 @@
 import os
 
-import requests
 from kodi_six import xbmc
 
 from .log import log
-from .mem_cache import cached
 from .constants import ADDON_PROFILE, ADDON_ID, COMMON_ADDON
 
 def get_dns_rewrites(dns_rewrites=None):
@@ -21,51 +19,26 @@ def get_dns_rewrites(dns_rewrites=None):
 
     return rewrites
 
-@cached(expires=60*5)
-def _get_url(url):
-    log.debug('Request DNS URL: {}'.format(url))
-    return requests.get(url).text
-
 def _load_rewrites(directory):
+    file_path = os.path.join(xbmc.translatePath(directory), 'urls.txt')
+    if not os.path.exists(file_path):
+        return []
+
     rewrites = []
-
-    file_names = [
-        'urls.txt',
-        'dns_rewrites.txt', #legacy
-    ]
-
-    found = False
-    for name in file_names:
-        file_path = os.path.join(xbmc.translatePath(directory), name)
-        if os.path.exists(file_path):
-            found = True
-            break
-
-    if not found:
-        return rewrites
-
     try:
-        def _process_lines(lines):
-            for line in lines:
-                entry = line.strip()
-                if not entry or entry.startswith('#'):
-                    continue
-
-                entries = [x.strip() for x in entry.split() if x.strip()]
-                if len(entries) == 1 and entries[0].lower().startswith('http'):
-                    text = _get_url(entry)
-                    _process_lines(text.split('\n'))
-                    continue
-
-                if len(entries) < 2:
-                    continue
-
-                rewrites.append(entries)
-
         with open(file_path, 'r') as f:
             text = f.read()
 
-        _process_lines(text.split('\n'))
+        for line in text.split('\n'):
+            entry = line.strip()
+            if not entry or entry.startswith('#'):
+                continue
+
+            entries = [x.strip() for x in entry.split() if x.strip()]
+            if len(entries) < 2:
+                continue
+
+            rewrites.append(entries)
     except Exception as e:
         log.debug('DNS Rewrites Failed: {}'.format(file_path))
         log.exception(e)
