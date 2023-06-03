489d3281bb324f5fc33c0aa459b5473158abae9c
Matt Huisman <hi@matthuisman.nz>
2023-06-03 12:27:14 +1200

    script.module.slyguy -> v0.65.6

--- script.module.slyguy/resources/modules/slyguy/__init__.py
+++ script.module.slyguy/resources/modules/slyguy/__init__.py
@@ -1,8 +1,15 @@
 import sys
 
 from .log import log
+from .constants import ADDON_ID, COMMON_ADDON_ID, DEPENDENCIES_ADDON_ID
 
-if 'slyguy.dependencies' in sys.path[-1]:
-    new_path = sys.path[-3:] + sys.path[:-3]
-    log.debug('Changing Kodi 20+ wrong sys.path (xbmc/issues/22985)\nOld: {}\nNew: {}'.format(sys.path, new_path))
+if DEPENDENCIES_ADDON_ID in sys.path[-1]:
+    if ADDON_ID == DEPENDENCIES_ADDON_ID:
+        index = 1
+    elif ADDON_ID == COMMON_ADDON_ID:
+        index = 2
+    else:
+        index = 3
+    new_path = sys.path[-index:] + sys.path[:-index]
+    log.debug('Fix for wrong sys.path in Kodi 20+ (xbmc/issues/22985)\nOld: {}\nNew: {}'.format(sys.path, new_path))
     sys.path = new_path
