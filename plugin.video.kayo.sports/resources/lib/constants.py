91c09247e6108af139279ac94c552831f52bb35b
Matt Huisman <hi@matthuisman.nz>
2023-09-16 08:52:47 +1200

    plugin.video.kayo.sports -> v0.14.5

--- plugin.video.kayo.sports/resources/lib/constants.py
+++ plugin.video.kayo.sports/resources/lib/constants.py
@@ -1,5 +1,7 @@
+import random
+
 HEADERS = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/{}.0.0.{} Safari/537.36'.format(random.randint(90, 116), random.randint(0, 99)),
 }
 
 AUTH_HEADERS = {
