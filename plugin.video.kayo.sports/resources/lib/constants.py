d93933a68546633aa7034eea7c0aec86e6587bfd
Matt Huisman <hi@matthuisman.nz>
2023-09-19 18:38:02 +1200

    plugin.video.kayo.sports -> v0.14.8

--- plugin.video.kayo.sports/resources/lib/constants.py
+++ plugin.video.kayo.sports/resources/lib/constants.py
@@ -1,5 +1,5 @@
 HEADERS = {
-    'user-agent': 'okhttp/4.9.3',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
 }
 
 AUTH_URL = 'https://auth.streamotion.com.au/oauth'
