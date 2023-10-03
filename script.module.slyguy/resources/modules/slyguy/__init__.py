e1b253490e9e01c38f6ba441c113e2cc99d561ae
Matt Huisman <hi@matthuisman.nz>
2023-10-03 20:56:03 +1300

    script.module.slyguy -> v0.73.8

--- script.module.slyguy/resources/modules/slyguy/__init__.py
+++ script.module.slyguy/resources/modules/slyguy/__init__.py
@@ -1,20 +1,7 @@
 import sys
-import datetime
 
 from .log import log
 from .constants import ADDON_ID, COMMON_ADDON_ID, DEPENDENCIES_ADDON_ID
-
-# python embedded (as used in kodi) has a known bug for second calls of strptime.
-# The python bug is docmumented here https://bugs.python.org/issue27400
-# The following workaround patch is borrowed from https://forum.kodi.tv/showthread.php?tid=112916&pid=2914578#pid2914578
-class proxydt(datetime.datetime):
-    @staticmethod
-    def strptime(date_string, format):
-        import time
-        return datetime.datetime(*(time.strptime(date_string, format)[0:6]))
-
-log.debug('patching datetime.datetime')
-datetime.datetime = proxydt
 
 log.debug('sys.path: {}'.format(sys.path))
 if ADDON_ID not in sys.path[0]:
