913a2a3a830f767060b4d409cc7db5c33906adb7
Matt Huisman <hi@matthuisman.nz>
2023-10-26 07:08:09 +1300

    plugin.video.willow.tv -> v0.1.5

--- plugin.video.willow.tv/resources/lib/constants.py
+++ plugin.video.willow.tv/resources/lib/constants.py
@@ -1,18 +1,15 @@
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Linux; U; Android 5.1; en-US; SHIELD Android TV Build/LMY47D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 UCBrowser/10.5.2.582 U3/0.8.0 Mobile Safari/534.30',
-    'X-Forwarded-For': '72.229.28.185',
 }
 
-MD5_KEY            = '35a131404c264f36ca4031500143e4acf0682cd5'
-LOGIN_URL          = 'https://www.willow.tv/EventMgmt/webservices/mobi_auth.asp'
-LIVE_URL           = 'http://appfeeds.willow.tv/iptv/RokuLiveMatchList.json'
-PLAYBACK_URL       = 'http://www.willow.tv/iptvws/v1/playlist.asp'
-UPCOMING_URL       = 'http://appfeeds.willow.tv/RokuUpcoming.json'
-ARCHIVE_URL        = 'http://appfeeds.willow.tv/iptv/RokuSeriesList.json'
+MD5_KEY = '35a131404c264f36ca4031500143e4acf0682cd5'
+LOGIN_URL = 'https://www.willow.tv/EventMgmt/webservices/mobi_auth.asp'
+LIVE_URL = 'http://appfeeds.willow.tv/iptv/RokuLiveMatchList.json'
+PLAYBACK_URL = 'http://www.willow.tv/iptvws/v1/playlist.asp'
+UPCOMING_URL = 'http://appfeeds.willow.tv/RokuUpcoming.json'
+ARCHIVE_URL = 'http://appfeeds.willow.tv/iptv/RokuSeriesList.json'
 ARCHIVES_MATCH_URL = 'http://appfeeds.willow.tv/iptv/RokuMatchVODList_{match_id}.json'
-# COUNTRY_CODE_URL   = 'http://linsoftlayer.willow.tv/getCC.php'
-# HIGHLIGHTS_URL     = 'http://appfeeds.willow.tv/OttTrendingHighlights_{country_code}.json'
-TEAMS_IMAGE_URL    = 'https://aimages.willow.tv/AppleTvLogos/{team1}_{team2}.png' #.replace(" ","")
+TEAMS_IMAGE_URL = 'https://aimages.willow.tv/AppleTvLogos/{team1}_{team2}.png'
 
-DEV_TYPE           = 'androidtv'
-SERVICE_TIME       = 300
+DEV_TYPE = 'androidtv'
+SERVICE_TIME = 300
