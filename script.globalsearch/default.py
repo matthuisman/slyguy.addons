17654f87118b6e531fed5ced6b1f54b0febd6646
Matt Huisman <hi@matthuisman.nz>
2023-11-08 22:18:57 +1300

    script.globalsearch -> v99.0.8

--- script.globalsearch/default.py
+++ script.globalsearch/default.py
@@ -1,28 +1,3 @@
-import sys
+import xbmcgui
 
-from kodi_six import xbmc, xbmcaddon
-from six.moves.urllib.parse import unquote_plus
-
-from resources.lib import gui
-
-
-LANGUAGE = xbmcaddon.Addon().getLocalizedString
-CWD = xbmcaddon.Addon().getAddonInfo('path')
-
-if (__name__ == '__main__'):
-    try:
-        params = dict(arg.split('=') for arg in sys.argv[1].split('&'))
-    except:
-        params = {}
-
-    searchstring = unquote_plus(params.get('searchstring',''))
-    if searchstring:
-        del params['searchstring']
-    else:
-        searchstring = gui.search()
-        if not searchstring:
-            sys.exit(0)
-
-    ui = gui.GUI('script-globalsearch.xml', CWD, defaultSkin='default', defaultRes='1080i', isMedia=True, searchstring=searchstring, params=params)
-    ui.doModal()
-    del ui
+xbmcgui.Dialog().ok('[MOVED]', 'This addon has moved into the "SlyGuy Global Search+ Repository". That can be installed from the main SlyGuy Repository')
