09fb511f9feafa7935ee768f3043d9665abc954e
Matt Huisman <hi@matthuisman.nz>
2023-11-11 11:36:05 +1300

    script.globalsearch -> v99.0.10

--- script.globalsearch/default.py
+++ script.globalsearch/default.py
@@ -1,28 +1,27 @@
 import sys
 
-from kodi_six import xbmc, xbmcaddon
+from kodi_six import xbmcaddon
 from six.moves.urllib.parse import unquote_plus
 
-from resources.lib import gui
+from lib import gui
 
 
-LANGUAGE = xbmcaddon.Addon().getLocalizedString
 CWD = xbmcaddon.Addon().getAddonInfo('path')
 
-if (__name__ == '__main__'):
-    try:
-        params = dict(arg.split('=') for arg in sys.argv[1].split('&'))
-    except:
-        params = {}
 
-    searchstring = unquote_plus(params.get('searchstring',''))
-    if searchstring:
-        del params['searchstring']
-    else:
-        searchstring = gui.search()
-        if not searchstring:
-            sys.exit(0)
+try:
+    params = dict(arg.split('=') for arg in sys.argv[1].split('&'))
+except:
+    params = {}
 
-    ui = gui.GUI('script-globalsearch.xml', CWD, defaultSkin='default', defaultRes='1080i', isMedia=True, searchstring=searchstring, params=params)
-    ui.doModal()
-    del ui
+searchstring = unquote_plus(params.get('searchstring',''))
+if searchstring:
+    del params['searchstring']
+else:
+    searchstring = gui.search()
+    if not searchstring:
+        sys.exit(0)
+
+ui = gui.GUI('script-globalsearch.xml', CWD, 'default', '1080i', True, searchstring=searchstring, params=params)
+ui.doModal()
+del ui
