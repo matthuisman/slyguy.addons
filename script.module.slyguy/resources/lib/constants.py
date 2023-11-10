7c27465d98db66483f270a9c58c820d1f3ff79dd
Matt Huisman <hi@matthuisman.nz>
2023-11-11 12:49:54 +1300

    script.module.slyguy -> v0.74.8

--- script.module.slyguy/resources/lib/constants.py
+++ script.module.slyguy/resources/lib/constants.py
@@ -3,6 +3,6 @@ from slyguy.constants import REPO_DOMAIN
 ## NEWS ##
 NEWS_URL = REPO_DOMAIN+'/.repo/news.json.gz'
 ADDONS_URL = REPO_DOMAIN+'/.repo/addons.json.gz'
-ADDONS_MD5 = REPO_DOMAIN+'/.repo/addons.xml.md5'
+ADDONS_MD5 = REPO_DOMAIN+'/.repo/addons.json.md5'
 NEWS_CHECK_TIME = 1800 #30mins
 UPDATES_CHECK_TIME = 1800 #30mins
