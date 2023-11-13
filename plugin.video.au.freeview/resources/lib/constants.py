5fd36afaf9eadc8a3d23bdc767fe1fe8c112809a
Matt Huisman <hi@matthuisman.nz>
2023-11-13 20:05:32 +1300

    plugin.video.au.freeview -> v0.19.4

--- plugin.video.au.freeview/resources/lib/constants.py
+++ plugin.video.au.freeview/resources/lib/constants.py
@@ -1,3 +1,8 @@
 REGIONS = ['Sydney', 'Melbourne', 'Brisbane', 'Perth', 'Adelaide', 'Darwin', 'Hobart', 'Canberra']
-M3U8_URL = 'https://i.mjh.nz/au/{region}/tv.json.gz'
+DATA_URL = 'https://i.mjh.nz/au/{region}/tv.json.gz'
 EPG_URL = 'https://i.mjh.nz/au/{region}/epg.xml.gz'
+
+ALL = 0
+FREEVIEW_ONLY = 1
+FAST_ONLY = 2
+CHANNEL_MODES = [ALL, FREEVIEW_ONLY, FAST_ONLY]
--- plugin.video.au.freeview/resources/lib/plugin.py
+++ plugin.video.au.freeview/resources/lib/plugin.py
@@ -5,7 +5,7 @@ from slyguy import plugin, settings, inputstream
 from slyguy.mem_cache import cached
 from slyguy.session import Session
 
-from .constants import M3U8_URL, REGIONS, EPG_URL
+from .constants import *
 from .language import _
 
 @plugin.route('')
@@ -28,7 +28,6 @@ def live_tv(**kwargs):
 
     folder = plugin.Folder(_(_.REGIONS[region]))
     show_chnos = settings.getBool('show_chnos', False)
-    hide_fast = settings.getBool('hide_fast_channels', False)
 
     if settings.getBool('show_epg', True):
         now = arrow.now()
@@ -36,17 +35,10 @@ def live_tv(**kwargs):
     else:
         epg_count = None
 
-    for slug in sorted(channels, key=lambda k: (channels[k].get('chno') is None, channels[k].get('network', 'zzzzzzz') if not show_chnos else None, float(channels[k].get('chno', 'inf')), channels[k].get('name', 'zzzzzzz'))):
-        channel = channels[slug]
-        if hide_fast and not channel.get('chno'):
-            continue
-
+    for channel in get_channels(region):
         plot = u''
         count = 0
         if epg_count:
-            if channel.get('epg_id') and not channel.get('programs'):
-                channel['programs'] = channels.get(channel['epg_id'], {}).get('programs', [])
-
             for index, row in enumerate(channel.get('programs', [])):
                 start = arrow.get(row[0])
                 try: stop = arrow.get(channel['programs'][index+1][0])
@@ -63,7 +55,7 @@ def live_tv(**kwargs):
 
         item = plugin.Item(
             label = channel['name'],
-            path = plugin.url_for(play, slug=slug, _is_live=True),
+            path = plugin.url_for(play, region=region, slug=channel['slug'], _is_live=True),
             info = {'plot': plot},
             art = {'thumb': channel.get('logo'), 'fanart': channel.get('fanart')},
             playable = True,
@@ -77,9 +69,8 @@ def live_tv(**kwargs):
     return folder
 
 @plugin.route()
-def play(slug, **kwargs):
-    region = get_region()
-    channel = get_channels(region)[slug]
+def play(region, slug, **kwargs):
+    channel = get_channels(region, slug)
 
     item = plugin.Item(
         label = channel['name'],
@@ -99,10 +90,35 @@ def play(slug, **kwargs):
 
     return item
 
-@cached(60*5)
-def get_channels(region):
-    url = M3U8_URL.format(region=region)
-    return Session().gz_json(url)
+def get_channels(region, slug=None):
+    @cached(60*5)
+    def get_data():
+        url = DATA_URL.format(region=region)
+        return Session().gz_json(url)
+
+    channels = get_data()
+    if slug:
+        return channels[slug]
+
+    show_chnos = settings.getBool('show_chnos', False)
+    channel_mode = settings.getEnum('channel_mode', CHANNEL_MODES, default=ALL)
+
+    channel_list = []
+    for slug in sorted(channels, key=lambda k: (channels[k].get('chno') is None, channels[k].get('network', 'zzzzzzz') if not show_chnos else None, float(channels[k].get('chno', 'inf')), channels[k].get('name', 'zzzzzzz'))):
+        channel = channels[slug]
+        channel['slug'] = slug
+
+        if channel_mode == FREEVIEW_ONLY and not channel.get('chno'):
+            continue
+        elif channel_mode == FAST_ONLY and channel.get('chno'):
+            continue
+
+        if channel.get('epg_id') and not channel.get('programs'):
+            channel['programs'] = channels.get(channel['epg_id'], {}).get('programs', [])
+
+        channel_list.append(channel)
+
+    return channel_list
 
 def get_region():
     return REGIONS[settings.getInt('region_index')]
@@ -111,17 +127,10 @@ def get_region():
 @plugin.merge()
 def playlist(output, **kwargs):
     region = get_region()
-    channels = get_channels(region)
-    hide_fast = settings.getBool('hide_fast_channels', False)
-
     with codecs.open(output, 'w', encoding='utf8') as f:
         f.write(u'#EXTM3U x-tvg-url="{}"'.format(EPG_URL.format(region=region)))
 
-        for slug in sorted(channels, key=lambda k: (channels[k].get('chno') is None, channels[k].get('network', 'zzzzzzz'), float(channels[k].get('chno', 'inf')), channels[k].get('name', 'zzzzzzz'))):
-            channel = channels[slug]
-            if hide_fast and not channel.get('chno'):
-                continue
-
+        for channel in get_channels(region):
             f.write(u'\n#EXTINF:-1 channel-id="{channel_id}" tvg-id="{epg_id}" tvg-chno="{chno}" tvg-logo="{logo}",{name}\n{url}'.format(
-                channel_id=slug, epg_id=channel.get('epg_id', slug), logo=channel.get('logo', ''), name=channel['name'], chno=channel.get('chno', ''),
-                    url=plugin.url_for(play, slug=slug, _is_live=True)))
+                channel_id=channel['slug'], epg_id=channel.get('epg_id', channel['slug']), logo=channel.get('logo', ''), name=channel['name'], chno=channel.get('chno', ''),
+                    url=plugin.url_for(play, region=region, slug=channel['slug'], _is_live=True)))
