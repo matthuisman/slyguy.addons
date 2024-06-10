230c32228dc07ce2df3225001d6de4fd23f3765b
Matt Huisman <hi@matthuisman.nz>
2024-06-10 21:07:27 +1200

    script.module.slyguy -> v0.81.0

--- script.module.slyguy/resources/lib/proxy.py
+++ script.module.slyguy/resources/lib/proxy.py
@@ -1253,7 +1253,12 @@ class RequestHandler(BaseHTTPRequestHandler):
 
         base_url = urljoin(response.url, '/')
 
+        def relative_replace(match):
+            return match.group(0).replace(match.group(1), urljoin(response.url, match.group(1)))
+
         m3u8 = re.sub(r'^/', r'{}'.format(base_url), m3u8, flags=re.I|re.M)
+        m3u8 = re.sub('^(\.\./.*)$', relative_replace, m3u8, flags=re.I|re.M)
+        m3u8 = re.sub('URI="(\.\./.*)"', relative_replace, m3u8, flags=re.I|re.M)
         m3u8 = re.sub(r'URI="/', r'URI="{}'.format(base_url), m3u8, flags=re.I|re.M)
 
         ## Convert to proxy paths
