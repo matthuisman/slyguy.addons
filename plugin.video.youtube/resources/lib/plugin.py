5362932348fb1c301b9dfeefe2e91026bf79a068
Matt Huisman <hi@matthuisman.nz>
2024-06-14 23:06:06 +1200

    plugin.video.youtube -> v99.1.0

--- plugin.video.youtube/resources/lib/plugin.py
+++ plugin.video.youtube/resources/lib/plugin.py
@@ -2,12 +2,12 @@ import sys
 from collections import defaultdict
 
 from kodi_six import xbmc
-from six.moves.urllib_parse import unquote, parse_qsl
+from six.moves.urllib_parse import unquote
 
 from slyguy import plugin, settings, inputstream
 from slyguy.log import log
 from slyguy.util import get_system_arch
-from slyguy.constants import ADDON_PROFILE, ROUTE_CONTEXT
+from slyguy.constants import ADDON_PROFILE
 
 
 from .language import _
@@ -35,40 +35,9 @@ def play_android_apk(video_id):
     log.debug(start_activity)
     xbmc.executebuiltin(start_activity)
 
-@plugin.route(ROUTE_CONTEXT)
-def context(listitem, **kwargs):
-    vid_tag = listitem.getVideoInfoTag()
-    yt_url = vid_tag.getTrailer()
-    params = dict(parse_qsl(yt_url.split('?')[1]))
-    video_id = params.get('video_id') or params.get('videoid')
-    if not video_id:
-        raise plugin.PluginError(_(_.NO_VIDEO_ID_FOUND, url=yt_url))
-
-    li = _play(video_id)
-    li.label = u"{} ({})".format(listitem.getLabel(), _.TRAILER)
-    li.info = {
-        'plot': vid_tag.getPlot(),
-        'tagline': vid_tag.getTagLine(),
-        'year': vid_tag.getYear(),
-        'mediatype': vid_tag.getMediaType(),
-    }
-
-    try:
-        # v20+
-        li.info['genre'] = vid_tag.getGenres()
-    except AttributeError:
-        li.info['genre'] = vid_tag.getGenre()
-
-    for key in ['thumb','poster','banner','fanart','clearart','clearlogo','landscape','icon']:
-        li.art[key] = listitem.getArt(key)
-
-    return li
 
 @plugin.route('/play')
 def play(video_id, **kwargs):
-    return _play(video_id)
-
-def _play(video_id):
     log.debug("YouTube ID {}".format(video_id))
 
     is_android = get_system_arch()[0] == 'Android'
