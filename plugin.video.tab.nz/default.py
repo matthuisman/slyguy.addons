77520d672c2eadc64ae19a81974a393a71a9b167
Matt Huisman <hi@matthuisman.nz>
2024-08-10 21:13:06 +1200

    plugin.video.tab.nz -> v0.8.4

--- plugin.video.tab.nz/default.py
+++ plugin.video.tab.nz/default.py
@@ -1,5 +1,2 @@
-import sys
-
-from resources.lib.plugin import plugin
-
-plugin.dispatch(sys.argv[2])
+from slyguy.dialog import ok
+ok('This addon no longer works and has been deprecated. Open a github issue if you would like it to return')
