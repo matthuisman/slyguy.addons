210ee76e365da70d2c06d96f35c27652f4d73c9d
Matt Huisman <hi@matthuisman.nz>
2024-11-22 11:47:03 +1300

    script.module.slyguy -> v0.85.48

--- script.module.slyguy/service.py
+++ script.module.slyguy/service.py
@@ -14,9 +14,8 @@ except Exception as e:
         xbmc.log('Slyguy common updating. Expected error: {}'.format(e), xbmc.LOGINFO)
         sys.exit(0)
 
-    xbmc.log('Failed to import Slyguy common service', xbmc.LOGFATAL)
-    traceback.print_exc()
-    if xbmcgui.Dialog().ok('SlyGuy Error', 'Error importing Slyguy common service\nThis major bug is usually fixed very quickly\n[B]Click OK to check for updates[/B]'):
+    xbmc.log('Failed to import Slyguy common service: {}'.format(traceback.format_exc()), xbmc.LOGERROR)
+    if xbmcgui.Dialog().ok('SlyGuy Error', 'Error starting Slyguy common service\nThis major bug is usually fixed very quickly\n[B]Click OK to check for updates[/B]'):
         xbmc.executebuiltin('UpdateAddonRepos')
 else:
     service.run()
