69e2522d7993cef35b42b866ee41803009d5b63b
Matt Huisman <hi@matthuisman.nz>
2025-02-04 22:32:27 +1300

    plugin.program.iptv.merge -> v0.28.1

--- plugin.program.iptv.merge/resources/lib/merger.py
+++ plugin.program.iptv.merge/resources/lib/merger.py
@@ -11,7 +11,7 @@ from six.moves.urllib.parse import unquote_plus, quote_plus
 
 from slyguy import database, gui, userdata
 from slyguy.log import log
-from slyguy.util import remove_file, hash_6, FileIO, gzip_extract, xz_extract, run_plugin, safe_copy, unique, makedirs
+from slyguy.util import remove_file, hash_6, FileIO, gzip_extract, xz_extract, run_plugin, safe_copy, unique
 from slyguy.session import Session, gdrivedl
 from slyguy.constants import ADDON_PROFILE, CHUNK_SIZE
 from slyguy.exceptions import Error
--- plugin.program.iptv.merge/resources/lib/settings.py
+++ plugin.program.iptv.merge/resources/lib/settings.py
@@ -1,5 +1,5 @@
 from slyguy.constants import ADDON, ADDON_ID, KODI_VERSION
-from slyguy.settings import CommonSettings
+from slyguy.settings import CommonSettings, is_donor
 from slyguy.settings.types import Bool, Text, Browse, Number, Action
 
 from .language import _
@@ -7,6 +7,7 @@ from .language import _
 
 class Settings(CommonSettings):
     OUTPUT_DIR = Browse('output_dir', _.OUTPUT_DIR, type=Browse.DIRECTORY, default=ADDON.getAddonInfo('profile'), use_default=False)
+    GZ_EPG = Bool('gz_epg', _.GZ_EPG, default=False, visible=KODI_VERSION >= 18, enable=is_donor, disabled_reason=_.SUPPORTER_ONLY)
     MERGE_EVERY_X = Bool('auto_merge', _.MERGE_EVERY_X, default=True, disabled_value=None, enable=lambda: not Settings.MERGE_AT_HOUR.value)
     X_HOURS = Number('reload_time_hours', _.X_HOURS, default=12, lower_limit=1, upper_limit=48, visible=lambda: Settings.MERGE_EVERY_X.value)
 
@@ -24,7 +25,6 @@ class Settings(CommonSettings):
     PAGE_SIZE = Number('page_size', _.PAGE_SIZE, default=200)
     ASK_TO_ADD = Bool('ask_to_add', _.ASK_TO_ADD, default=False)
     IPTV_MERGE_PROXY = Bool('iptv_merge_proxy', _.IPTV_MERGE_PROXY, default=True)
-    GZ_EPG = Bool('gz_epg', _.GZ_EPG, default=False, visible=KODI_VERSION >= 18)
 
 
 settings = Settings()
