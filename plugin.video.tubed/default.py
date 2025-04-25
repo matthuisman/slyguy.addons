7a6ef8b8443602ad85f0272356e78538b77c8711
Matt Huisman <hi@matthuisman.nz>
2025-04-25 12:25:13 +1200

    plugin.video.tubed -> v99.2.22

--- plugin.video.tubed/default.py
+++ plugin.video.tubed/default.py
@@ -6,8 +6,6 @@ addon_id = xbmcaddon.Addon().getAddonInfo('id')
 
 url = sys.argv[0] + sys.argv[2]
 new_url = url.replace(addon_id, trailer_addon_id)
-
-xbmc.executebuiltin('InstallAddon({})'.format(trailer_addon_id), True)
 xbmc.log("{} - Re-routing {} -> {}".format(addon_id, url, new_url))
 
 li = xbmcgui.ListItem(path=new_url)
