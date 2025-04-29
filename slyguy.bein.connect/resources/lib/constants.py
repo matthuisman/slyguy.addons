454411b25c84496e29757e7a9832108a3b677af2
Matt Huisman <hi@matthuisman.nz>
2025-04-29 21:26:32 +1200

    slyguy.bein.connect -> v0.3.1

--- slyguy.bein.connect/resources/lib/constants.py
+++ slyguy.bein.connect/resources/lib/constants.py
@@ -6,7 +6,7 @@ APP_DATA_URL = 'https://d3ruzby390gxsc.cloudfront.net/init/bein_connect.json'
 SECRET_KEY = '2H6kATruFruW&&e5_u4u'
 REGION = 'mena'
 PLAYER_NAME = 'androiddashapp'
-APP_VERSION_URL = 'https://i.mjh.nz/bein/mena.version'
+APP_VERSION_URL = 'https://i.mjh.nz/.apk/mena.version'
 
 LOGIN_CONNECT = 0
 LOGIN_SATELLITE = 1
